20cdffc5c12c108c59431f663ce4edcd3462de14
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-08-06 12:14:03 -0700

    add UI score element and rolling player state

--- js/player.js
+++ js/player.js
@@ -1,4 +1,4 @@
-import { Sitting, Running, Jumping, Falling } from "./playerStates.js";
+import { Sitting, Running, Jumping, Falling, Rolling } from "./playerStates.js";
 
 export class Player {
   constructor(game) {
@@ -23,11 +23,13 @@ export class Player {
       new Running(this),
       new Jumping(this),
       new Falling(this),
+      new Rolling(this),
     ];
     this.currentState = this.states[0];
     this.currentState.enter();
   }
   update(input, deltaTime) {
+    this.checkCollision();
     this.currentState.handleInput(input);
     // horizontal movement
     this.x += this.speed;
@@ -51,6 +53,8 @@ export class Player {
     }
   }
   draw(context) {
+    if (this.game.debug)
+      context.strokeRect(this.x, this.y, this.width, this.height);
     context.drawImage(
       this.image,
       this.frameX * this.width,
@@ -70,5 +74,21 @@ export class Player {
     this.currentState = this.states[state];
     this.game.speed = this.game.maxSpeed * speed;
     this.currentState.enter();
+  }
+  checkCollision() {
+    this.game.enemies.forEach((enemy) => {
+      if (
+        enemy.x < this.x + this.width &&
+        enemy.x + enemy.width > this.x &&
+        enemy.y < this.y + this.height &&
+        enemy.y + enemy.height > this.y
+      ) {
+        // collision detected
+        enemy.markedForDeletion = true;
+        this.game.score++;
+      } else {
+        // no collision
+      }
+    });
   }
 }
