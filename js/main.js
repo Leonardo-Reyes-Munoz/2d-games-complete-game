d9aefd820d8ad8bac8b4389015c5d2bb9c6792ec
Leonardo-Reyes-Munoz <reyesleonardoa@live.com>
2023-08-06 13:14:23 -0700

    add particle.js and particle logic

--- js/main.js
+++ js/main.js
@@ -22,11 +22,15 @@ window.addEventListener("load", function () {
       this.input = new InputHandler(this);
       this.UI = new UI(this);
       this.enemies = [];
+      this.particles = [];
+      this.maxParticles = 50;
       this.enemyTimer = 0;
       this.enemyInterval = 1000;
       this.debug = true;
       this.score = 0;
       this.fontColor = "black";
+      this.player.currentState = this.player.states[0];
+      this.player.currentState.enter();
     }
     update(deltaTime) {
       this.background.update();
@@ -43,12 +47,24 @@ window.addEventListener("load", function () {
         if (enemy.markedForDeletion)
           this.enemies.splice(this.enemies.indexOf(enemy), 1);
       });
+      // handle particles
+      this.particles.forEach((particle, index) => {
+        particle.update();
+        if (particle.markedForDeletion) this.particles.splice(index, 1);
+      });
+      if (this.particles.length > this.maxParticles) {
+        this.particles = this.particles.slice(0, this.maxParticles);
+      }
+      console.log(this.particles);
     }
     draw(context) {
       this.background.draw(context);
       this.player.draw(context);
       this.enemies.forEach((enemy) => {
         enemy.draw(context);
+      });
+      this.particles.forEach((particle) => {
+        particle.draw(context);
       });
       this.UI.draw(context);
     }
@@ -57,12 +73,11 @@ window.addEventListener("load", function () {
         this.enemies.push(new GroundEnemy(this));
       else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
       this.enemies.push(new FlyingEnemy(this));
-      console.log(this.enemies);
     }
   }
 
   const game = new Game(canvas.width, canvas.height);
-  console.log(game);
+
   let lastTime = 0;
 
   function animate(timeStamp) {
